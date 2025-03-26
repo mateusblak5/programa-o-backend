956396415c4e55e2122cd77abc0c743fb6c107cd
mateusblak5 <mateus.da.rosa@escola.pr.gov.br>
2025-03-26 19:41:14 +0000

    utilizando print

---
+++ src/main/java/com/mycompany/Aula01Variaveis.java
@@ -0,0 +1,14 @@
+package com.mycompany;
+
+public class Aula01Variaveis {
+
+    public static void main(String[] args) {
+        //Variavel logica
+      
+        //Tipo primitivo
+        boolean valorLogicoPrimitivo = true;
+        System.out.println("O valor da variavel valorLogicoAbstrato é:" + valorLogicoAbstrato);
+        //tipo abstrato
+        boolean valorLogicoAbstrato = false;
+    }
+} 
