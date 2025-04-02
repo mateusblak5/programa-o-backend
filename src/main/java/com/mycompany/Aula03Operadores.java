dd7c5a857d0fec47a6faa0a8b5e747b68a0b262b
mateusblak5 <mateus.da.rosa@escola.pr.gov.br>
2025-04-02 19:40:32 +0000

    atividade 2/04/25

---
+++ src/main/java/com/mycompany/Aula03Operadores.java
@@ -0,0 +1,34 @@
+package com.mycompany;
+
+public class Aula03Operadores {
+     Run|Debug
+     public static void main(String[] args) {
+        /**
+         * tipos de comentario
+         */
+
+        // Comentario de uma linha
+
+         /**
+          * Comentarios de
+          * multiplas
+          * linhas
+          */
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     }
+}
