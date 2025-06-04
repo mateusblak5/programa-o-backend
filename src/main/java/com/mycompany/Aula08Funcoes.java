768d78574d7e5fceafe7e8cf103f199b899031dc
mateusblak5 <mateus.da.rosa@escola.pr.gov.br>
2025-06-04 19:33:25 +0000

    aula 08 funções recursivas

--- src/main/java/com/mycompany/Aula08Funcoes.java
+++ src/main/java/com/mycompany/Aula08Funcoes.java
@@ -6,12 +6,29 @@ public class Aula08Funcoes {
 
         System.out.println("--- Tabuada do" + numero + "---");
 
-        for (int i = 1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             System.out.println(numero + " x " + i + " = " + multiplicacao(numero, i));
         }
+
+        System.out.println(fatorial(numero));
+
     }
 
-    public static int multiplicacao(int num, int num2){
+    public static int multiplicacao(int num, int num2) {
         return num * num2;
+    }
+
+    /**
+     * Funções recursivas
+     * uma função recursiva faz uma chamada para ela própria
+     * porém, toda recursividade deve ter um ponto de parada,
+     * caso contrário pode resultar em um loop infinito
+     */
+    public static int fatorial(int n) {
+        if (n != 0) {
+            return n * fatorial(n - 1);
+        } else {
+            return 1;
+        }
     }
 }
