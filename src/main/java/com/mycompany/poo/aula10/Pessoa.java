7bae0a83c4e478c79375a88466117b9331120717
mateusblak5 <mateus.da.rosa@escola.pr.gov.br>
2025-08-06 13:41:46 +0000

    Aula 10, modificadores de acesso, parte 2

--- src/main/java/com/mycompany/poo/aula10/Pessoa.java
+++ src/main/java/com/mycompany/poo/aula10/Pessoa.java
@@ -24,4 +24,24 @@ public class Pessoa {
         System.out.println("idade: " + idade);
         System.out.println("Altura (m): " + altura);
     }
+
+    /**
+     * nomeclatura de metodos
+     * 
+     * metodos/funçoes indicam açoes e por padrao utilizamos verbos.
+     * 
+     * Ex: correr, andar, caminhar ...
+     */
+
+    public void atribuirSalario (double valor) {
+        this.salario = valor;
+
+        System.out.println("o novo salario de" + this.nome + "é :" + this.salario);
+    }
+
+    public void atribuirSalario(double valor, double bonus) {
+        this.salario =valor + bonus;
+
+        System.out.println("o novo salário de" + this.nome + "é :" + this.salario + "mais um bonus de" + bonus);
+    }
 }
