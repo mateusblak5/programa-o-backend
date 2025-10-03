4a3ab294d9f7a0efe2d32b11fd9c4fef7ce595d7
mateusblak5 <mateus.da.rosa@escola.pr.gov.br>
2025-10-03 14:35:07 +0000

    aula 13

---
+++ src/main/java/com/mycompany/poo/aula13/App.java
@@ -0,0 +1,7 @@
+package com.mycompany.poo.aula13;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/com/mycompany/poo/aula13/animais/Animal.java
@@ -0,0 +1,10 @@
+package com.mycompany.poo.aula13.animais;
+
+/**
+ * Classe Abstrata
+ * 
+ * Toda classe abstrata
+ */
+public abstract class Animal {
+    
+}
